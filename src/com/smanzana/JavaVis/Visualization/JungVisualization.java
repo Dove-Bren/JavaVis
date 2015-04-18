d8bca57aeba358b3435bc5ada703d5c8b769a4db
Skyler <skymanzanares@hotmail.com>
2015-04-17 23:36:20 -0600

    Added a  bunch of JMenu and JFrame objects to add a search bar and action

--- src/com/smanzana/JavaVis/Visualization/JungVisualization.java
+++ src/com/smanzana/JavaVis/Visualization/JungVisualization.java
@@ -1,13 +1,21 @@
 package com.smanzana.JavaVis.Visualization;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Paint;
+import java.awt.event.ActionEvent;
 import java.awt.event.MouseEvent;
+import java.beans.PropertyChangeListener;
 
+import javax.swing.AbstractAction;
+import javax.swing.Action;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
+import javax.swing.JOptionPane;
+import javax.swing.JPopupMenu;
+import javax.swing.JToolBar;
 
 import org.apache.commons.collections15.Transformer;
 
@@ -25,6 +33,7 @@ import edu.uci.ics.jung.visualization.VisualizationViewer;
 import edu.uci.ics.jung.visualization.control.AbstractPopupGraphMousePlugin;
 import edu.uci.ics.jung.visualization.control.DefaultModalGraphMouse;
 import edu.uci.ics.jung.visualization.control.ModalGraphMouse.Mode;
+import edu.uci.ics.jung.visualization.decorators.ToStringLabeller;
 
 public class JungVisualization {
 	
@@ -34,17 +43,17 @@ public class JungVisualization {
 	
 	private static final Paint enumColor = Color.RED;
 	
-	private static final class PopupPlugin extends AbstractPopupGraphMousePlugin {
+	private static final class SearchPlugin extends AbstractPopupGraphMousePlugin {
 
 		@Override
 		protected void handlePopup(MouseEvent arg0) {
 			// TODO Auto-generated method stub
-			System.out.println("omg");
+			//JFrame popup = new JPopupMenu("Search");
 		}
 		
 		@Override
 		public void mouseReleased(MouseEvent e) {
-			if (e.getButton() == MouseEvent.BUTTON1) {
+			if (e.getButton() == MouseEvent.BUTTON3) {
 				handlePopup(e);
 			}
 //			else {
@@ -53,6 +62,34 @@ public class JungVisualization {
 		}
 		
 	}
+	
+	private static final class SearchAction extends AbstractAction {
+		
+		private JungVisualization vis;
+		
+		boolean enabled;
+		
+		public SearchAction(JungVisualization vis) {
+			super("Search", null);
+			this.vis = vis;
+			enabled = true;
+		}
+		
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			vis.performSearch();
+		}
+
+		
+	}
+	
+	private JFrame window;
+	
+	private String searchTerm;
+	
+	private VisualizationViewer<GraphNode, DirectedWeightedEdge> vv;
+	
+	private Layout<GraphNode, DirectedWeightedEdge> layout;
 
 	public void Visualize(Graph graph) {
 		// TODO Auto-generated method stub
@@ -73,12 +110,11 @@ public class JungVisualization {
 		}
 	
 	    // The Layout<V, E> is parameterized by the vertex and edge types
-	    Layout<GraphNode, DirectedWeightedEdge> layout = new CircleLayout<GraphNode, DirectedWeightedEdge>(visGraph);
-	    layout.setSize(new Dimension(300,300)); // sets the initial size of the space
+	    layout = new CircleLayout<GraphNode, DirectedWeightedEdge>(visGraph);
+	    layout.setSize(new Dimension(600,600)); // sets the initial size of the space
 	     // The BasicVisualizationServer<V,E> is parameterized by the edge types
-	     VisualizationViewer<GraphNode, DirectedWeightedEdge> vv = 
-	              new VisualizationViewer<GraphNode, DirectedWeightedEdge>(layout);
-	     vv.setPreferredSize(new Dimension(350,350)); //Sets the viewing area size
+	     vv = new VisualizationViewer<GraphNode, DirectedWeightedEdge>(layout);
+	     vv.setPreferredSize(new Dimension(700,700)); //Sets the viewing area size
 	     
 	     Transformer<GraphNode,Paint> vertexPaint = new Transformer<GraphNode, Paint>() {
 	         public Paint transform(GraphNode node) {
@@ -97,6 +133,15 @@ public class JungVisualization {
 	         }
 	     };  
 	     
+	     Transformer<GraphNode, String> vertexLabel = new Transformer<GraphNode, String>() {
+	    	 public String transform(GraphNode node) {
+	    		 if (node == null || node.getCclass() == null) {
+	    			 return "";
+	    		 }
+	    		 return node.getCclass().getName();
+	    	 }
+	     };
+	     
 //	     Transformer<GraphNode, String> vertexTooltip = new Transformer<GraphNode, String>(){
 //	    	public String transform(GraphNode node) {
 //	    		if (node == null || node.getCclass() == null) {
@@ -113,19 +158,28 @@ public class JungVisualization {
 //	     };
 	     
 	     vv.getRenderContext().setVertexFillPaintTransformer(vertexPaint);
+	     vv.setVertexToolTipTransformer(new ToStringLabeller<GraphNode>());
+	     vv.getRenderContext().setVertexLabelTransformer(vertexLabel);
 	     //vv.getRenderContext().setVer(vertexTooltip);
 	     
 	     
 	     //create mouse
 	     DefaultModalGraphMouse mouse = new DefaultModalGraphMouse();
 	     mouse.setMode(Mode.TRANSFORMING);
-	     mouse.add(new PopupPlugin());
+	     mouse.add(new SearchPlugin());
 	     vv.addKeyListener(mouse.getModeKeyListener());
 	     vv.setGraphMouse(mouse);
 	     
-	     JFrame frame = new JFrame("Simple Graph View");
-	     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	     frame.getContentPane().add(vv); 
+	     window = new JFrame("Simple Graph View");
+	     window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	     
+
+	     JToolBar toolbar = new JToolBar();
+	     toolbar.add(new SearchAction(this));
+	     window.getContentPane().setLayout(new BorderLayout());
+	     window.getContentPane().add(toolbar, BorderLayout.PAGE_START);
+	     
+	     window.getContentPane().add(vv, BorderLayout.CENTER); 
 	     
 
 	     
@@ -136,16 +190,39 @@ public class JungVisualization {
 	     modeMenu.setIcon(null); // I'm using this in a main menu
 	     modeMenu.setPreferredSize(new Dimension(80,20)); // Change the size 
 	     menuBar.add(modeMenu);
-	     frame.setJMenuBar(menuBar);
+	     
+	     window.setJMenuBar(menuBar);
 	     
 	     
-	     frame.pack();
-	     frame.setVisible(true);     
+	     window.pack();
+	     window.setVisible(true);     
 	}
 	
 	public void Visualize(Tree tree) {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	
+	private void performSearch() {
+		if (window == null) {
+			return;
+		}
+		if (searchTerm == null) {
+			searchTerm = "";
+		}
+		searchTerm = (String) JOptionPane.showInputDialog(window, "Search for:", searchTerm);
+		
+		if (searchTerm == null) {
+			return;
+		}
+		
+		GraphNode result = null;
+		for (GraphNode node : layout.getGraph().getVertices()) {
+			if (node.getCclass().getName().equals(searchTerm)) {
+				//vv.set
+			}
+		}
 	}
 
 }
